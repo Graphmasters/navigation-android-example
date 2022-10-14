98e738838275fcf737e4f341077b0395b91d43cb
marcin <marcin.konopelko@graphmasters.net>
2022-10-14 14:06:46 +0200

    #14 :construction: Wip

--- app/src/main/java/net/graphmasters/navigation/example/MainActivity.kt
+++ app/src/main/java/net/graphmasters/navigation/example/MainActivity.kt
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
+import android.media.AudioManager
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -52,7 +53,6 @@ import net.graphmasters.multiplatform.navigation.vehicle.VehicleConfig
 import net.graphmasters.multiplatform.navigation.ui.camera.CameraSdk
 import net.graphmasters.multiplatform.navigation.ui.camera.CameraUpdate
 import net.graphmasters.multiplatform.navigation.ui.camera.NavigationCameraHandler
-import net.graphmasters.multiplatform.navigation.ui.detach.OffRouteDetachStateProvider
 import net.graphmasters.multiplatform.navigation.ui.locale.LanguageProvider
 import net.graphmasters.multiplatform.navigation.ui.voice.instructions.NavigationVoiceInstructionHandler
 import net.graphmasters.multiplatform.navigation.ui.voice.instructions.VoiceInstructionDispatcher
@@ -157,6 +157,26 @@ class MainActivity : AppCompatActivity(), LocationListener,
             this, Manifest.permission.ACCESS_FINE_LOCATION
         ) == PackageManager.PERMISSION_GRANTED
 
+    private fun initVoiceInstructionHandler() {
+        this.initAudioComponent()
+
+        this.voiceInstructionHandler = NavigationVoiceInstructionHandler(
+            this.navigationSdk,
+            this.createVoiceInstructionStringGenerator(
+                Locale.getDefault()),
+            this.createVoiceInstructionDispatcher()
+        )
+    }
+
+    private fun initAudioComponent() {
+        AudioComponent.Companion.init(
+            this,
+            object : AudioConfigProvider {
+                override val audioConfig: AudioConfig
+                    get() = createAudioConfig()
+            })
+    }
+
     private fun createVoiceInstructionStringGenerator(
         locale: Locale,
     ): VoiceInstructionStringGenerator = LocaleVoiceInstructionStringGenerator(
@@ -168,68 +188,34 @@ class MainActivity : AppCompatActivity(), LocationListener,
         }
     )
 
-    private fun createAudioPlayer() =
-        ConfigurableAudioPlayer(this, object : AudioConfigProvider {
-            override val audioConfig: AudioConfig
-                get() = AudioConfig(
-                    desiredOutputType = AudioConfig.OutputType.DEVICE,
-                    audioStream = 0,
-                    playbackDelay = null,
-                    appVolume = 60,
-                    focusType = AudioConfig.FocusType.NONE,
-                    showVolumeUi = false)
-        })
-
     private fun createVoiceInstructionDispatcher() =
         object : VoiceInstructionDispatcher {
             override fun dispatch(voiceInstruction: List<String>, onDone: (String) -> Unit) {
                 voiceInstruction.forEach {
                     Log.d(TAG, it)
-                    createAudioPlayer().execute(TextToSpeechJobFactory.create(
-                        context,
-                        it,
-                        AudioConfig(
-                            desiredOutputType = AudioConfig.OutputType.DEVICE,
-                            audioStream = 0,
-                            playbackDelay = null,
-                            appVolume = 60,
-                            focusType = AudioConfig.FocusType.NONE,
-                            showVolumeUi = false
-                        )
-                    ))
+                    createAudioPlayer().playTextToSpeech(
+                        text = it,
+                        config = null,
+                        cancelable = false,
+                        priority = 1L,
+                        callback = null
+                    )
                 }
             }
         }
 
-    private fun initAudioComponent() {
-        AudioComponent.Companion.init(
-            this,
-            object : AudioConfigProvider {
-                override val audioConfig: AudioConfig
-                    get() = AudioConfig(
-                        desiredOutputType = AudioConfig.OutputType.DEVICE,
-                        audioStream = 0,
-                        playbackDelay = null,
-                        appVolume = 0,
-                        focusType = AudioConfig.FocusType.NONE,
-                        showVolumeUi = false)
-            })
-    }
+    private fun createAudioPlayer() = //Todo: create it only once
+        ConfigurableAudioPlayer(this, object : AudioConfigProvider {
+            override val audioConfig: AudioConfig
+                get() = createAudioConfig()
+        })
 
+    private fun createAudioConfig() =
+        AudioConfig(
+            desiredOutputType = AudioConfig.OutputType.DEVICE,
+            audioStream = AudioManager.STREAM_MUSIC)
 
     private lateinit var voiceInstructionHandler: VoiceInstructionHandler
-
-    private fun initVoiceInstructionHandler() {
-        this.initAudioComponent()
-
-        this.voiceInstructionHandler = NavigationVoiceInstructionHandler(
-            this.navigationSdk,
-            this.createVoiceInstructionStringGenerator(
-            Locale.getDefault()),
-            this.createVoiceInstructionDispatcher()
-        )
-    }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -495,8 +481,6 @@ class MainActivity : AppCompatActivity(), LocationListener,
     override fun onNavigationStarted(routable: Routable) {
         Toast.makeText(this, "onNavigationStarted", Toast.LENGTH_SHORT).show()
         this.voiceInstructionHandler.enabled = true
-        val t = this.voiceInstructionHandler.enabled
-        val s = t
         Log.d(TAG, "onNavigationStarted $routable")
     }
 
@@ -504,6 +488,7 @@ class MainActivity : AppCompatActivity(), LocationListener,
         Toast.makeText(this, "onNavigationStopped", Toast.LENGTH_SHORT).show()
         Log.d(TAG, "onNavigationStopped")
 
+        this.voiceInstructionHandler.enabled = false
         this.cameraMode = CameraMode.FREE
         this.navigationInfoCard.visibility = View.GONE
     }
